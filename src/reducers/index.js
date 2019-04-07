41fb5cf64e3f351b9bb08f1d646d76573431a260
Ixax Tavarez <i.tavarez@umiami.edu>
2019-04-07 21:05:43 +0000

    content created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import postsReducer from './postsRreducer';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
     posts: postsReducer
